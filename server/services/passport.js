5a2e00abf5bbe0e041cc957cd08fa00a0c4d9868
Arnesh Pal <arneshpal000@gmail.com>
2025-08-29 17:27:39 +0530

    Update passport.js

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,4 +1,3 @@
-// server/services/passport.js
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const LocalStrategy = require('passport-local').Strategy;
@@ -7,10 +6,12 @@ const db = require('../models');
 const User = db.User;
 
 passport.serializeUser((user, done) => done(null, user.id));
+
 passport.deserializeUser((id, done) => {
     User.findByPk(id).then(user => done(null, user));
 });
 
+// Google OAuth Strategy
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -18,6 +19,8 @@ passport.use(new GoogleStrategy({
     proxy: true
 }, async (accessToken, refreshToken, profile, done) => {
     try {
+        // This logic is now simplified to only find or create the user.
+        // Default data will be created by the API routes on demand.
         const [user] = await User.findOrCreate({
             where: { googleId: profile.id },
             defaults: {
@@ -32,20 +35,17 @@ passport.use(new GoogleStrategy({
     }
 }));
 
-// NEW: Local Email/Password Strategy
-passport.use(new LocalStrategy({ usernameField: 'email' },
+// Local Email/Password Strategy
+passport.use(new LocalStrategy({ usernameField: 'email' }, 
     async (email, password, done) => {
         try {
             const user = await User.findOne({ where: { email: email } });
-            if (!user) {
-                return done(null, false, { message: 'Incorrect email.' });
-            }
-            if (!user.password) { // User signed up with Google
-                return done(null, false, { message: 'Please sign in with Google.' });
+            if (!user || !user.password) {
+                return done(null, false, { message: 'Incorrect credentials.' });
             }
             const isMatch = await bcrypt.compare(password, user.password);
             if (!isMatch) {
-                return done(null, false, { message: 'Incorrect password.' });
+                return done(null, false, { message: 'Incorrect credentials.' });
             }
             return done(null, user);
         } catch (err) {
